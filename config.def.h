705fb2aa97b7ac1c60d3ff8548a0d606466a8950
mhashemm <msakr456@gmail.com>
2022-09-03 23:08:07 +0200

    rules

--- config.def.h
+++ config.def.h
@@ -29,10 +29,10 @@ static const Rule rules[] = {
 	 */
 	/* class      instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
 	// { "Gimp",     NULL,     NULL,           0,         1,          0,           0,        -1 },
-	{ "Chromium", NULL,     NULL,           0,         1,          0,          -1,        -1 },
+	// { "Chromium", NULL,     NULL,           0,         1,          0,          -1,        -1 },
 	{ "St",       NULL,     NULL,           0,         0,          1,           0,        -1 },
 	{ NULL,       NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
-	{ "mpv",      NULL,     NULL,           0,         0,          1,          -1,        -1 }
+	{ NULL,       NULL,     "mpv",          0,         0,          1,           0,        -1 }
 };
 
 /* layout(s) */
