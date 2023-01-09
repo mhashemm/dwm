9b1039a983c1b74b65a437e2061300d65c6edc6a
mhashemm <msakr456@gmail.com>
2023-01-09 09:13:34 +0200

    change zoom key

--- config.def.h
+++ config.def.h
@@ -86,7 +86,7 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_d,             incnmaster,        {.i = -1 } },
 	{ MODKEY,                       XK_h,             setmfact,          {.f = -0.05} },
 	{ MODKEY,                       XK_l,             setmfact,          {.f = +0.05} },
-	{ MODKEY|ShiftMask,             XK_Return,        zoom,              {0} },
+	{ MODKEY,                       XK_equal,         zoom,              {0} },
 	{ MODKEY,                       XK_Tab,           view,              {0} },
 	{ MODKEY,                       XK_BackSpace,     killclient,        {0} },
 	{ MODKEY,                       XK_f,             togglefloating,    {0} },
