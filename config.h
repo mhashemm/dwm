1ed3b61089f9201a7cba56beb26d64e494622ed4
mhashemm <msakr456@gmail.com>
2023-01-10 00:20:42 +0200

    Revert "swallow"
    This reverts commit 5c9695be4d62c8006b63920241ae0bef6d7eabe2.

--- config.h
+++ config.h
@@ -3,7 +3,6 @@
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=12", "NotoColorEmoji:pixelsize=10:antialias=true:autohint=true" };
@@ -27,14 +26,11 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance  title                    tags mask  isfloating  isterminal  noswallow  monitor */
-	{ "st",       NULL,     NULL,                    0,         0,          1,           0,        -1 },
-	{ NULL,       NULL,     "Event Tester",          0,         0,          0,           1,        -1 }, /* xev */
-	{ NULL,       NULL,     "Picture in picture",    0,         1,          0,           0,        -1 },
-	{ NULL,       NULL,     "DevTools",              0,         1,          0,           0,        -1 },
-	{ "feh",      NULL,     NULL,                    0,         0,          0,           0,        -1 },
-	{ "mpv",      NULL,     NULL,                    0,         0,          0,           0,        -1 },
-	{ "Deadbeef", NULL,     NULL,                    1<<8,      0,          0,           1,        -1 },
+	/* class      instance  title                    tags mask  isfloating  monitor */
+	{ NULL,       NULL,     "Picture in picture",    0,         1,          -1 },
+	{ NULL,       NULL,     "DevTools",              0,         1,          -1 },
+	{ "Deadbeef", NULL,     NULL,                    1<<8,      0,          -1 },
+
 };
 
 /* layout(s) */
